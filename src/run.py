5c97f5b9e3d0a081d42f08416fe5f99207b2b0e0
Abhishek Sharma <sharm271@data.cs.purdue.edu>
2019-09-01 20:32:10 -0400

    initializing hyperparams

--- src/run.py
+++ src/run.py
@@ -25,16 +25,42 @@ from __future__ import division
 
 import time
 import math
-import pickle
 from docopt import docopt
 
 import torch
 import torch.nn.functional as F
 
+from vocab import Vocab
+from utils import read_corpus
+
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 def train(args):
-#TODO
+    """
+    train our neural model
+    @param args (dict): command line args
+    """
+    train_data_src = read_corpus(args['--train-src'], domain='src')
+    train_data_tgt = read_corpus(args['--train-tgt'], domain='tgt')
+
+    train_data = list(zip(train_data_src, train_data_tgt))
+
+    train_batch_size = int(args['--batch-size'])
+    clip_grad = float(args['--clip-grad'])
+    model_save_path = args['--save-model-to']
+
+    vocab = Vocab.load(args['--vocab'])
+
+    model = Seq2Seq()
+    model.train()
+    model = model.to(device)
+
+    init_lr = float(args['--lr'])
+    optimizer = torch.optim.Adam(model.parameters(), lr=init_lr)
+
+    begin_time = time.time()
+    for epoch in range(int(args['--max-epoch'])):
+        #TODO
 
 if __name__ == "__main__":
     args = docopt(__doc__)
