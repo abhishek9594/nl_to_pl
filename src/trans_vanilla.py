464a48754c902929a214132427ea6d6cc808227e
Abhishek Sharma <sharm271@data.cs.purdue.edu>
2019-10-28 09:58:55 -0400

    adding pos embedding

--- src/trans_vanilla.py
+++ src/trans_vanilla.py
@@ -8,17 +8,19 @@ import torch.nn.functional as F
 from trans_encoder import TransEncoder
 from trans_decoder import TransDecoder
 from model_embeddings import ModelEmbeddings
+from positional_embeddings import PositionalEmbeddings
 from utils import subsequent_mask, clone
 
 class TransVanilla(nn.Module):
 
     def __init__(self, embed_size, hidden_size, vocab, dropout_rate):
         super(TransVanilla, self).__init__()
-        self.embeddings = ModelEmbeddings(embed_size, vocab)
         self.d_model = embed_size
         self.d_ff = hidden_size
         self.dropout_rate = dropout_rate
         self.vocab = vocab
+        self.embeddings = ModelEmbeddings(self.d_model, self.vocab)
+        self.pe = PositionalEmbeddings(self.d_model, self.dropout_rate)
         
         self.encoder_blocks = clone(TransEncoder(self.d_model, self.d_ff, self.dropout_rate), n=6)
         self.decoder_blocks = clone(TransDecoder(self.d_model, self.d_ff, self.dropout_rate), n=6)
@@ -49,13 +51,13 @@ class TransVanilla(nn.Module):
         return scores
         
     def encode(self, src, src_mask=None):
-        x = self.embeddings.src_embedding(src)
+        x = self.pe(self.embeddings.src_embedding(src))
         for encoder in self.encoder_blocks:
             x = encoder(x, src_mask)
         return x
 
     def decode(self, src_encoded, tgt, src_mask=None, tgt_mask=None):
-        x = self.embeddings.tgt_embedding(tgt)
+        x = self.pe(self.embeddings.tgt_embedding(tgt))
         for decoder in self.decoder_blocks:
             x = decoder(src_encoded, x, src_mask, tgt_mask)
         return self.vocab_project(x)
