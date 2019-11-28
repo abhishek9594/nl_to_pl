95391a316a25d24ddc1538d71a8a679fb1d4cb93
Abhishek Sharma <sharm271@data.cs.purdue.edu>
2019-11-27 20:38:04 -0500

    fixing IfExp and adding parser for some expressions

--- src/preParser.py
+++ src/preParser.py
@@ -31,7 +31,17 @@ def parseOp(op):
     elif isinstance(op, ast.BitAnd):
         return 'BitAnd'
     else:
-        return 'FloorDiv' 
+        return 'FloorDiv'
+
+def parseUnOp(unop):
+    if isinstance(unop, ast.Invert):
+        return 'Invert'
+    elif isinstance(unop, ast.Not):
+        return 'Not'
+    elif isinstance(unop, ast.UAdd):
+        return 'UAdd'
+    else:
+        return 'USub'
 
 def parseCmpOp(cmpop):
     if isinstance(cmpop, ast.Eq):
@@ -67,6 +77,12 @@ def parseBin(exp):
     """
     return parseOp(exp.op) + ' ( ' + parseExp(exp.left) + ' ' + parseExp(exp.right) + ' )'
 
+def parseUn(exp):
+    """
+    UnaryOp(unaryop op, expr operand)
+    """
+    return parseUnOp(exp.op) + ' ' + parseExp(exp.operand)
+
 def parseLambda(exp):
     """
     Lambda(arguments args, expr body)
@@ -77,7 +93,7 @@ def parseIf(exp):
     """
     IfExp(expr test, expr body, expr orelse)
     """
-    return parseExp(test) + ' ' + parseExp(body) + ' ' + parseExp(orelse)
+    return parseExp(exp.test) + ' ' + parseExp(exp.body) + ' ' + parseExp(exp.orelse)
 
 def parseDict(exp):
     """
@@ -85,17 +101,35 @@ def parseDict(exp):
     """
     return 'Dict ( ' + ' '.join([parseExp(key) for key in exp.keys]) + ' ' + ' '.join([parseExp(value) for value in exp.values]) + ' )'
 
+def parseSet(exp):
+    """
+    Set(expr* elts)
+    """
+    return 'Set ( ' + ' '.join([parseExp(elt) for elt in exp.elts]) + ' )'
+
 def parseListComp(exp):
     """
     ListComp(expr elt, comprehension* generators)
     """
     return 'List ( ' + parseExp(exp.elt) + ' ' + ' '.join([parseComp(gen) for gen in exp.generators]) + ' )'
 
+def parseSetComp(exp):
+    """
+    SetComp(expr elt, comprehension* generators)
+    """
+    return 'Set ( ' + parseExp(exp.elt) + ' ' + ' '.join([parseComp(gen) for gen in exp.generators]) + ' )'
+
 def parseDictComp(exp):
     """
     DictComp(expr key, expr value, comprehension* generators)
     """
     return 'Dict ( ' + '( ' + parseExp(exp.key) + ' ' + parseExp(exp.value) + ' ) ' + ' '.join([parseComp(gen) for gen in exp.generators]) + ' )'
+
+def parseGenExp(exp):
+    """
+    GeneratorExp(expr elt, comprehension* generators)
+    """
+    return parseExp(exp.elt) + ' ' + ' '.join([parseComp(gen) for gen in exp.generators])
 
 def parseYield(exp):
     """
@@ -121,6 +155,12 @@ def parseNum(exp):
     Num(object n)
     """
     return str(exp.n)
+
+def parseRep(exp):
+    """
+    Repr(expr value)
+    """
+    return parseExp(exp.value)
 
 def parseStr(exp):
     """
@@ -159,7 +199,20 @@ def parseTup(exp):
     return 'Tup ( ' + ' '.join([parseExp(elt) for elt in exp.elts]) + ' ) '
 
 def parseSlice(exp):
-    return parseExp(exp.value)
+    """
+    Slice(expr? lower, expr? upper, expr? step)
+    | ExtSlice(slice* dims)
+    | Index(expr value)
+    """
+    if hasattr(exp, 'lower'):
+        lower = parseExp(exp.lower) + ' ' if exp.lower else ''
+        upper = parseExp(exp.upper) + ' ' if exp.upper else ''
+        step = parseExp(exp.step) + ' ' if exp.step else ''
+        return '( ' + lower + upper + step + ' )'
+    elif hasattr(exp, 'dims'):
+        return ' '.join([parseSlice(dim) for dim in exp.dims])
+    else:
+        return parseExp(exp.value)
 
 def parseArgs(args):
     """
@@ -195,22 +248,32 @@ def parseExp(exp):
         return parseBool(exp)
     elif isinstance(exp, ast.BinOp):
         return parseBin(exp)
+    elif isinstance(exp, ast.UnaryOp):
+        return parseUn(exp)
     elif isinstance(exp, ast.Lambda):
         return parseLambda(exp)
     elif isinstance(exp, ast.IfExp):
         return 'If ' + parseIf(exp)
     elif isinstance(exp, ast.Dict):
         return parseDict(exp)
+    elif isinstance(exp, ast.Set):
+        return parseSet(exp)
     elif isinstance(exp, ast.ListComp):
         return parseListComp(exp)
+    elif isinstance(exp, ast.SetComp):
+        return parseSetComp(exp)
     elif isinstance(exp, ast.DictComp):
         return parseDictComp(exp)
+    elif isinstance(exp, ast.GeneratorExp):
+        return parseGenExp(exp)
     elif isinstance(exp, ast.Yield):
         return parseYield(exp)
     elif isinstance(exp, ast.Compare):
         return parseCmp(exp)
     elif isinstance(exp, ast.Call):
         return parseFunCall(exp)
+    elif isinstance(exp, ast.Repr):
+        return parseRep(exp)
     elif isinstance(exp, ast.Num):
         return parseNum(exp)
     elif isinstance(exp, ast.Str):
@@ -226,6 +289,7 @@ def parseExp(exp):
     elif isinstance(exp, ast.Tuple):
         return parseTup(exp)
     else:
+        #matches all the above case for 2.7.17 version
         return ' '
 
 def parseSent(pl_sent):
