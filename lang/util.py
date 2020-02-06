db8c8a329868bdb5ece80df96f2a4a6f5e7cf929
Abhishek Sharma <sharm271@data.cs.purdue.edu>
2020-02-05 22:15:40 -0500

    removing formatting functions lang.util

--- lang/util.py
+++ lang/util.py
@@ -6,43 +6,6 @@ def typename(x):
         return x
     return x.__name__
 
-def escape(text):
-    text = text \
-        .replace('"', '-``-') \
-        .replace('\'', '-`-') \
-        .replace(' ', '-SP-') \
-        .replace('\t', '-TAB-') \
-        .replace('\n', '-NL-') \
-        .replace('\r', '-NL2-') \
-        .replace('(', '-LRB-') \
-        .replace(')', '-RRB-') \
-        .replace('|', '-BAR-')
-
-    if text is None:
-        return '-NONE-'
-    elif text == '':
-        return '-EMPTY-'
-
-    return text
-
-def unescape(text):
-    if text == '-NONE-':
-        return None
-
-    text = text \
-        .replace('-``-', '"') \
-        .replace('-`-', '\'') \
-        .replace('-SP-', ' ') \
-        .replace('-TAB-', '\t') \
-        .replace('-NL-', '\n') \
-        .replace('-NL2-', '\r') \
-        .replace('-LRB-', '(') \
-        .replace('-RRB-', ')') \
-        .replace('-BAR-', '|') \
-        .replace('-EMPTY-', '')
-
-    return text
-
 def parse_rule(rule):
     """
     rule = head_node_tp -> child1_tp[label1?] child2_tp[label2?] ...
