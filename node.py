0ba667d112f2bc54d89c9029699496e818080cae
Abhishek Sharma <sharm271@data.cs.purdue.edu>
2020-02-11 23:43:13 -0500

    adding Node and Rule to track nodes and rules vocab, similar style Vocab

---
+++ node.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python
+"""
+node.py: Map all the node types of the PL grammar to node_id
+
+Usage:
+    node.py --train-src=<file> --train-tgt=<file> NODE_FILE [options]
+
+Options:
+    -h --help                  Show this screen.
+    --train-code=<file>        File containing all the code
+"""
+
+from collections import Counter
+from docopt import docopt
+from itertools import chain
+import pickle
+
+from utils import read_corpus
+from parse import parse
+from lang.py.grammar import PY_AST_NODE_FIELDS
+
+class Node(object):
+    def __init__(self, node2id=None):
+        """
+        @param node2id (dict): dictionary mapping nodes -> indices
+        """
+        if node2id:
+            self.node2id = node2id
+        else:
+            self.node2id = dict()
+            self.node2id['<pad>'] = 0       #Pad Token
+        self.id2node = {v: k for k, v in self.node2id.items()}
+
+    def __getitem__(self, node):
+        """ Retrieve node's index.
+        @param node (str): node to look up.
+        @returns index (int): index of the node 
+        """
+        return self.node2id.get(node)
+
+    def __contains__(self, node):
+        """ Check if node is captured by Node.
+        @param node (str): node to look up
+        @returns contains (bool): whether node is contained    
+        """
+        return node in self.node2id
+
+    def __setitem__(self, key, value):
+        """ Raise error, if one tries to edit the Node.
+        """
+        raise ValueError('Node dictionary is readonly')
+
+    def __len__(self):
+        """ Compute number of nodes in Node.
+        @returns len (int): number of nodes in Node
+        """
+        return len(self.node2id)
+
+    def __repr__(self):
+        """ Representation of Node to be used
+        when printing the object.
+        """
+        return 'Node[size=%d]' % len(self)
+
+    def id2node(self, n_id):
+        """ Return mapping of index to node.
+        @param n_id (int): node index
+        @returns node (str): node corresponding to index
+        """
+        return self.id2node[n_id]
+
+    def add(self, node):
+        """ Add node to Node, if it is previously unseen.
+        @param node (str): node to add to Node
+        @return index (int): index that the node has been assigned
+        """
+        if node not in self:
+            n_id = self.node2id[node] = len(self)
+            self.id2node[n_id] = node
+            return n_id
+        else:
+            return self[node]
+
+    def nodes2indices(self, sents):
+        """ Convert list of nodes or list of sentences of nodes
+        into list or list of list of indices.
+        @param sents (list[str] or list[list[str]]): sentence(s) in nodes
+        @return node_ids (list[int] or list[list[int]]): sentence(s) in indices
+        """
+        if type(sents[0]) == list:
+            return [[self[node] for node in sent] for sent in sents]
+        else:
+            sent = sents
+            return [self[node] for node in sent]
+
+    def indices2nodes(self, node_ids):
+        """ Convert list of indices into nodes.
+        @param node_ids (list[int]): list of node ids
+        @return sents (list[str]): list of nodes
+        """
+        return [self.id2node[n_id] for n_id in node_ids]
+
+    @staticmethod
+    def build(corpus):
+        """ Given a corpus of list of list of nodes construct a Node obj.
+        @param corpus (list[list[str]]): corpus of data_nodes constructed in the main
+        @returns nodes (Node): Node instance produced from provided corpus
+        """
+        nodes = Node()
+        node_freq = Counter(chain(*corpus))
+        top_nodes = sorted(node_freq.keys(), key=lambda n: node_freq[n], reverse=True)
+        for node in top_nodes:
+            nodes.add(node)
+        for node in PY_AST_NODE_FIELDS:
+            if node not in nodes: nodes.add(node)
+        return nodes
+
+    def save(self, file_path):
+        """ Save Node to file as pickle dump.
+        @param file_path (str): file path to node file
+        """
+        pickle.dump(self.node2id, open(file_path, 'wb'))
+
+    @staticmethod
+    def load(file_path):
+        """
+        @param file_path (str): file path to node file
+        @returns Node object loaded from pickle dump
+        """
+        node2id = pickle.load(open(file_path, 'rb'))
+
+        return Node(node2id)
+
+if __name__ == '__main__':
+    args = docopt(__doc__)
+
+    print('read in source sentences: %s' % args['--train-src'])
+    print('read in target sentences: %s' % args['--train-tgt'])
+
+    (_, tgt_sents), _ = read_corpus(args['--train-src'], args['--train-tgt'])
+    tgt_tokens = [parse(code).to_tokens() for code in tgt_sents]
+    #filter GenTokens
+    data_nodes = [[token for token in tokens if 'GenToken' not in token] for tokens in tgt_tokens]
+
+    nodes = Node.build(data_nodes)
+    print('generated nodes: %d' % (len(nodes)))
+
+    nodes.save(args['NODE_FILE'])
+    print('nodes saved to %s' % args['NODE_FILE'])
---
+++ rule.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python
+"""
+rule.py: Map all the rule types of the PL grammar to rule_id
+
+Usage:
+    rule.py --train-src=<file> --train-tgt=<file> RULE_FILE [options]
+
+Options:
+    -h --help                  Show this screen.
+    --train-code=<file>        File containing all the code
+"""
+
+from collections import Counter
+from docopt import docopt
+from itertools import chain
+import pickle
+
+from utils import read_corpus
+from parse import parse
+
+class Rule(object):
+    def __init__(self, rule2id=None):
+        """
+        @param rule2id (dict): dictionary mapping rules -> indices
+        """
+        if rule2id:
+            self.rule2id = rule2id
+        else:
+            self.rule2id = dict()
+            self.rule2id['<pad>'] = 0       #Pad Token
+        self.id2rule = {v: k for k, v in self.rule2id.items()}
+
+    def __getitem__(self, rule):
+        """ Retrieve rule's index.
+        @param rule (str): rule to look up.
+        @returns index (int): index of the rule 
+        """
+        return self.rule2id.get(rule)
+
+    def __contains__(self, rule):
+        """ Check if rule is captured by Rule.
+        @param rule (str): rule to look up
+        @returns contains (bool): whether rule is contained    
+        """
+        return rule in self.rule2id
+
+    def __setitem__(self, key, value):
+        """ Raise error, if one tries to edit the Rule.
+        """
+        raise ValueError('Rule dictionary is readonly')
+
+    def __len__(self):
+        """ Compute number of rules in Rule.
+        @returns len (int): number of rules in Rule
+        """
+        return len(self.rule2id)
+
+    def __repr__(self):
+        """ Representation of Rule to be used
+        when printing the object.
+        """
+        return 'Rule[size=%d]' % len(self)
+
+    def id2rule(self, n_id):
+        """ Return mapping of index to rule.
+        @param n_id (int): rule index
+        @returns rule (str): rule corresponding to index
+        """
+        return self.id2rule[n_id]
+
+    def add(self, rule):
+        """ Add rule to Rule, if it is previously unseen.
+        @param rule (str): rule to add to Rule
+        @return index (int): index that the rule has been assigned
+        """
+        if rule not in self:
+            n_id = self.rule2id[rule] = len(self)
+            self.id2rule[n_id] = rule
+            return n_id
+        else:
+            return self[rule]
+
+    def rules2indices(self, sents):
+        """ Convert list of rules or list of sentences of rules
+        into list or list of list of indices.
+        @param sents (list[str] or list[list[str]]): sentence(s) in rules
+        @return rule_ids (list[int] or list[list[int]]): sentence(s) in indices
+        """
+        if type(sents[0]) == list:
+            return [[self[rule] for rule in sent] for sent in sents]
+        else:
+            sent = sents
+            return [self[rule] for rule in sent]
+
+    def indices2rules(self, rule_ids):
+        """ Convert list of indices into rules.
+        @param rule_ids (list[int]): list of rule ids
+        @return sents (list[str]): list of rules
+        """
+        return [self.id2rule[n_id] for n_id in rule_ids]
+
+    @staticmethod
+    def build(corpus):
+        """ Given a corpus of list of list of rules construct a Rule obj.
+        @param corpus (list[list[str]]): corpus of data_rules constructed in the main
+        @returns rules (Rule): Rule instance produced from provided corpus
+        """
+        rules = Rule()
+        rule_freq = Counter(chain(*corpus))
+        top_rules = sorted(rule_freq.keys(), key=lambda r: rule_freq[r], reverse=True)
+        for rule in top_rules:
+            rules.add(rule)
+        return rules
+
+    def save(self, file_path):
+        """ Save Rule to file as pickle dump.
+        @param file_path (str): file path to rule file
+        """
+        pickle.dump(self.rule2id, open(file_path, 'wb'))
+
+    @staticmethod
+    def load(file_path):
+        """
+        @param file_path (str): file path to rule file
+        @returns Rule object loaded from pickle dump
+        """
+        rule2id = pickle.load(open(file_path, 'rb'))
+
+        return Rule(rule2id)
+
+if __name__ == '__main__':
+    args = docopt(__doc__)
+
+    print('read in source sentences: %s' % args['--train-src'])
+    print('read in target sentences: %s' % args['--train-tgt'])
+
+    (_, tgt_sents), _ = read_corpus(args['--train-src'], args['--train-tgt'])
+    tgt_tokens = [parse(code).to_rules() for code in tgt_sents]
+    #filter GenTokens
+    data_rules = [[token for token in tokens if 'GenToken' not in token] for tokens in tgt_tokens]
+
+    rules = Rule.build(data_rules)
+    print('generated rules: %d' % (len(rules)))
+
+    rules.save(args['RULE_FILE'])
+    print('rules saved to %s' % args['RULE_FILE'])
