fbf59e238ee479ca2b67a61cbb0bd21bfb42cd35
Abhishek Sharma <sharm271@data.cs.purdue.edu>
2020-05-03 20:20:08 -0400

    adding token embedding inference step

--- models/trans_vanilla.py
+++ models/trans_vanilla.py
@@ -49,7 +49,7 @@ class TransVanilla(nn.Module):
         tgt_encoded = self.decode(src_encoded, tgt_in_nodes, tgt_in_tokens, src_mask, tgt_mask)
 
         tgt_rules_pred = F.log_softmax(self.rule_project(tgt_encoded), dim=-1)
-        tgt_rules_idx = self.rules.sents2Tensor(tgt_actions).to(self.device) #(b, max_tgt_sent)
+        tgt_rules_idx = self.rules.rules2Tensor(tgt_actions).to(self.device) #(b, max_tgt_sent)
         tgt_rules_mask = (tgt_rules_idx != padx).float()
         loss_rules = torch.gather(tgt_rules_pred, dim=-1,
                                     index=tgt_rules_idx.unsqueeze(-1)).squeeze(-1) * tgt_rules_mask
@@ -98,7 +98,7 @@ class TransVanilla(nn.Module):
         src_encoded = self.encode(src_in, src_mask=None)
 
         explore_nodes = ['<start>']
-        tgt_nodes = []
+        tgt_nodes, tgt_tokens = [], ['<pad>']
         actions = []
         while len(explore_nodes) > 0:
             if grammar.mul_cardinality(explore_nodes[-1]):
@@ -107,13 +107,15 @@ class TransVanilla(nn.Module):
                 curr_node = explore_nodes.pop()
             tgt_nodes.append(curr_node)
 
-            tgt_in = self.nodes.sents2Tensor([tgt_nodes]).to(self.device)
-            tgt_mask = subsequent_mask(tgt_in.shape[-1]).byte().to(self.device)
-            tgt_encoded = self.decode(src_encoded, tgt_in, src_mask=None, tgt_mask=tgt_mask)
+            tgt_in_nodes = self.nodes.nodes2Tensor([tgt_nodes]).to(self.device)
+            tgt_in_tokens = self.vocab.tgt.sents2Tensor([tgt_tokens]).to(self.device)
+            tgt_mask = subsequent_mask(tgt_in_nodes.shape[-1]).byte().to(self.device)
+            tgt_encoded = self.decode(src_encoded, tgt_in_nodes, tgt_in_tokens, src_mask=None, tgt_mask=tgt_mask)
             if grammar.node_prim_type(curr_node):
                 tgt_toks_pred = F.log_softmax(self.gen_tok_project(tgt_encoded), dim=-1)[:, -1, :] #extract last pred token
                 top_tok_id = tgt_toks_pred.argmax().item()
                 actions.append(GenTokenAction(self.vocab.tgt.id2word[top_tok_id]))
+                tgt_tokens.append(self.vocab.tgt.id2word[top_tok_id])
             else:
                 #composite_type => rule
                 rules_out = self.rule_project(tgt_encoded)[:, -1, :] #(b, R)
@@ -137,67 +139,9 @@ class TransVanilla(nn.Module):
                             node_name += '*'
                         action_nodes.append(node_name)
                     explore_nodes.extend(action_nodes[::-1])
+                tgt_tokens.append('<pad>')
         
         return actions
-        #old code
-        """
-        src_padded = self.vocab.src.sents2Tensor([src_sent]).to(self.device)
-        src_encoded = self.encode(src_padded, src_mask=None)
-
-        t = 0
-
-        hyps = [['<start>']]
-        completed_hyps = []
-        hyp_scores = torch.zeros(len(hyps), dtype=torch.float).to(self.device)
-
-        while len(completed_hyps) < beam_size and t < max_decoding_time_step:
-            num_hyp = len(hyps)
-            src_encoded_batch = src_encoded.expand(num_hyp, src_encoded.shape[1], src_encoded.shape[2])
-            x = self.vocab.tgt.sents2Tensor(hyps).to(self.device)
-            tgt_mask = subsequent_mask(x.shape[-1]).byte().to(self.device)
-            tgt_decoded = self.decode(src_encoded_batch, x, src_mask=None, tgt_mask=tgt_mask)
-            P = F.log_softmax(tgt_decoded, dim=-1)[:, -1, :] #extract last predicted word
-            
-            num_live_hyp = beam_size - len(completed_hyps)
-            live_hyp_scores = (hyp_scores.unsqueeze(1).expand_as(P) + P).view(-1) #shape = (num_live_hyp * len(vocab.tgt))
-            top_word_scores, top_word_pos = torch.topk(live_hyp_scores, k=num_live_hyp)
-
-            prev_hyp_ids = top_word_pos / len(self.vocab.tgt)
-            hyp_word_ids = top_word_pos % len(self.vocab.tgt)
-
-            new_hyps = []
-            live_hyp_ids = []
-            new_hyp_scores = []
-
-            for prev_hyp_id, hyp_word_id, top_word_score in zip(prev_hyp_ids, hyp_word_ids, top_word_scores):
-                prev_hyp_id = prev_hyp_id.item()
-                hyp_word_id = hyp_word_id.item()
-                top_word_score = top_word_score.item()
-
-                hyp_word = self.vocab.tgt.id2word[hyp_word_id]
-                new_hyp_sent = hyps[prev_hyp_id] + [hyp_word]
-                if hyp_word == '<eos>':
-                    completed_hyps.append((new_hyp_sent[1:-1], top_word_score))
-                else:
-                    new_hyps.append(new_hyp_sent)
-                    live_hyp_ids.append(prev_hyp_id)
-                    new_hyp_scores.append(top_word_score)
-
-            hyps = new_hyps
-            
-            live_hyp_ids = torch.tensor(live_hyp_ids, dtype=torch.long).to(self.device)
-            hyp_scores = torch.tensor(new_hyp_scores, dtype=torch.float).to(self.device)
-
-            t += 1
-        #end-while
-        #in this case best_hyp is not guaranteed
-        if len(completed_hyps) == 0:
-            completed_hyps.append((hyps[0][1:], hyp_scores[0].item()))
-
-        completed_hyps.sort(key=lambda (hyp, score): score, reverse=True)
-        best_hyp = [str(word) for word in completed_hyps[0][0]]
-        return best_hyp
-        """
 
     @property
     def device(self):
